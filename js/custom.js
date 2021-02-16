ba03e6308fa747a5d30bf499e5b6fa629270e2e7
ccls <vwali3@gmail.com>
2021-02-16 20:19:28 +0530

    upcode

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,19 @@ AOS.init({
   easing: 'ease-out',
 })
   // aos animation function end
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 30) {
+     $('.header-main-block').addClass('fixed');
+  } else {
+     $('.header-main-block').removeClass('fixed');
+  }
+}); 
+
+
+$(".header-main-block .navbar-icon").click(function(){
+  $(".header-main-block .navbar-icon").toggleClass("close");
+  $(".block-right-header").toggleClass("active");
+  $(".overflow-bg").toggleClass("active");
+  $("body").toggleClass("overflow");
+}) 
